d7aaa5d4fbfef40c9f3ecc7370ed23936c823602
Kappa <kappa@entropi.kr>
2025-03-23 01:05:01 +0900

    Added uid column on inbounds_mailboxes m2m relationship so an inbound to mailbox have a unique id for the mailbox

--- migrations/000001_initial.up.sql
+++ migrations/000001_initial.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    id         bigint primary key,
+    id         int8 primary key,
     local      varchar(64)              not null unique,
     created_at timestamp with time zone not null default now()
 );
@@ -8,24 +8,25 @@ CREATE TABLE users
 CREATE TABLE keys
 (
     value varchar(128) not null unique,
-    owner bigint       null references users (id)
+    owner int8         null references users (id)
 );
 
 CREATE TYPE mailbox_type AS ENUM ('inbox', 'junk', 'sent', 'drafts', 'trash', 'user');
 
 CREATE TABLE mailboxes
 (
-    id           bigint primary key,
+    id           int8 primary key,
     name         varchar(255)             not null,
     display_name varchar(255)             null,
-    owner        bigint                   null references users (id),
+    owner        int8                     null references users (id),
     type         mailbox_type             not null default 'user',
-    created_at   timestamp with time zone not null default now()
+    created_at   timestamp with time zone not null default now(),
+    unique (owner, name)
 );
 
 CREATE TABLE inbounds
 (
-    id           bigint primary key,
+    id           int8 primary key,
     content      text,
     sender       varchar(320)             not null,
     delivered_at timestamp with time zone not null default now()
@@ -33,7 +34,10 @@ CREATE TABLE inbounds
 
 CREATE TABLE inbounds_mailboxes
 (
-    inbound bigint references inbounds (id),
-    mailbox bigint references mailboxes (id),
-    unique (inbound, mailbox)
+    inbound int8 references inbounds (id),
+    mailbox int8 references mailboxes (id),
+    uid     int4 default (select max(uid) + 1
+                          from inbounds_mailboxes),
+    unique (inbound, mailbox),
+    unique (uid, mailbox)
 );
